c8ca14265593d861aaa80c938cd6c9947481df0b
Haykoo07 <hayksetyan2000@gmail.com>
2025-03-22 17:07:27 +0400

    Add files via upload

---
+++ 19.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter The Number: ");
+    scanf("%d", &n);
+    int i;
+    int h;
+    for (i = 1; i <= n; i++) {
+        for (h = 1; h <= i; h++) {
+            printf("*");
+       }
+    printf("\n");
+    }
+
+}
